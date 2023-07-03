de28a6423f2d5b308f16fe554dc789c25c614681
bgyuee <127265942+bgyuee@users.noreply.github.com>
2023-07-03 18:02:29 +0900

    firts commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center p-4">
+        <h1 className="text-3xl font-bold">{"Gyuyeop's blog"}</h1>
+            <nav className="flex gap-4">
+                <Link href='/'>home</Link>
+                <Link href='/about'>about</Link>
+                <Link href='/posts'>posts</Link>
+                <Link href='/contact'>contact</Link>
+            </nav>
+    </header>
+  )
+}
